ffe05c8158d52eb775c87d8886b599f99cb5f5d8
Kracken914dt <mecagisk6666@gmail.com>
2024-11-20 00:27:51 -0500

    get startet, falta arreglar ruta

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -20,7 +20,7 @@ import "driver.js/dist/driver.css";
 import InitialQuestionsModal from './Modals/InitialQuestionsModal';
 
 const Dashboard = () => {
-  const [user] = useAuthState(auth);
+  const [user, userloading] = useAuthState(auth);
   const [isExpenseModalVisible, setIsExpenseModalVisible] = useState(false);
   const [isIncomeModalVisible, setIsIncomeModalVisible] = useState(false);
   const [transactions, setTransactions] = useState([]);
@@ -30,6 +30,13 @@ const Dashboard = () => {
   const [expenses, setExpenses] = useState(0);
   const navigate = useNavigate();
   const [showInitialQuestions, setShowInitialQuestions] = useState(false);
+
+  useEffect(() => {
+    if (userloading) return; // Esperar a que se cargue el estado de autenticación
+    if (!user) {
+      navigate('/start'); // Redirigir a la página de inicio si no hay usuario
+    }
+  }, [user, userloading, navigate]);
 
   useEffect(() => {
     console.log('Transacciones actuales:', transactions);
@@ -570,7 +577,6 @@ const Dashboard = () => {
               <div className="bg-white rounded-2xl p-6 shadow-lg">
                 <TransactionSearch
                   transactions={transactions}
-                  user={{ name: user?.displayName, email: user?.email }}
                   exportToCsv={exportToCsv}
                   fetchTransactions={fetchTransactions}
                   addTransaction={addTransaction}
